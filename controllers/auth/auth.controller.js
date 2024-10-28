8db99dbb08a9a0bb2cba1a79bcae6605585f15ef
Asad Babayev <esedbabayev12@gmail.com>
2024-10-28 21:07:55 +0400

    update message

--- controllers/auth/auth.controller.js
+++ controllers/auth/auth.controller.js
@@ -8,6 +8,16 @@ export const signUp = async (request, response) => {
   const { userName, email, password } = request.body;
 
   try {
+    const checkExistingUser = await User.findOne({ email });
+
+    if (checkExistingUser) {
+      return response.json({
+        success: false,
+        message:
+          "Email is already in use. Please use a different email address.",
+      });
+    }
+
     const hashPassword = await bcrypt.hash(password, 10);
 
     const newUser = new User({
@@ -17,17 +27,59 @@ export const signUp = async (request, response) => {
     });
 
     await newUser.save();
-    response.status(200).json({ message: "sign up succesful" });
+    response
+      .status(200)
+      .json({ success: true, message: "Account created successfully" });
   } catch (error) {
     console.log(error);
-    response.status(500).json({ message: "an error occured" });
+    response.status(500).json({ success: false, message: "An error occured" });
   }
 };
 
 export const login = async (request, response) => {
+  const { email, password } = request.body;
+
   try {
+    const checkExistingUser = await User.findOne({ email });
+
+    if (!checkExistingUser)
+      return response.json({
+        success: false,
+        message: "Wrong email or password",
+      });
+
+    const isCorrectPassword = await bcrypt.compare(
+      password,
+      checkExistingUser.password
+    );
+
+    if (!isCorrectPassword)
+      return response.json({
+        success: false,
+        message: "Wrong email or password",
+      });
+
+    const token = jwt.sign(
+      {
+        id: checkExistingUser._id,
+        role: checkExistingUser.role,
+        email: checkExistingUser.email,
+      },
+      "CLIENT_SECRET_KEY",
+      { expiresIn: "60m" }
+    );
+
+    response.cookie("token", token, { httpOnly: true, secure: false }).json({
+      success: true,
+      message: "Logged in successfully",
+      user: {
+        id: checkExistingUser._id,
+        role: checkExistingUser.role,
+        email: checkExistingUser.email,
+      },
+    });
   } catch (error) {
     console.log(error);
-    response.status(500).json({ message: "an error occured" });
+    response.status(500).json({ success: false, message: "An error occured" });
   }
 };
