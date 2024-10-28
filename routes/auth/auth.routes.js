c2c8b248f13929d0b8b22c39e740eda17d7f7903
Asad Babayev <esedbabayev12@gmail.com>
2024-10-29 02:49:29 +0400

    create routes for logout & auth mw

--- routes/auth/auth.routes.js
+++ routes/auth/auth.routes.js
@@ -1,11 +1,27 @@
-import express from "express";
+import express, { response } from "express";
 
 // Controllers
-import { signUp, login } from "../../controllers/auth/auth.controller.js";
+import {
+  signUp,
+  login,
+  logout,
+} from "../../controllers/auth/auth.controller.js";
+
+// Middleware
+import { authMiddleware } from "../../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/sign-up", signUp);
 router.post("/sign-in", login);
+router.post("/logout", logout);
+router.get("/check-auth", authMiddleware, (request, response) => {
+    const user = request.user
+    response.status(200).json({
+        success: true,
+        message: "Authentication granted",
+        user
+    })
+})
 
 export default router;
