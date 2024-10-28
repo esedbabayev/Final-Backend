80e506042f93b7e78216674ffca3732dbfbc0c7a
Asad Babayev <esedbabayev12@gmail.com>
2024-10-28 19:34:21 +0400

    add sign in route

--- routes/auth/auth.routes.js
+++ routes/auth/auth.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 
 // Controllers
-import { signUp } from "../../controllers/auth/auth.controller.js";
+import { signUp, login } from "../../controllers/auth/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/sign-up", signUp);
+router.post("/sign-in", login);
 
 export default router;
