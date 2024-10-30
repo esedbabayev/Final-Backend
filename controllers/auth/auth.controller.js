45a861b4fdce6aa6cba25e1a19c1363ca6e9d368
Asad Babayev <esedbabayev12@gmail.com>
2024-10-30 23:35:58 +0400

    change expiry time

--- controllers/auth/auth.controller.js
+++ controllers/auth/auth.controller.js
@@ -66,7 +66,7 @@ export const login = async (request, response) => {
         email: checkExistingUser.email,
       },
       "CLIENT_SECRET_KEY",
-      { expiresIn: "60m" }
+      { expiresIn: "15d" }
     );
 
     response.cookie("token", token, { httpOnly: true, secure: false }).json({
