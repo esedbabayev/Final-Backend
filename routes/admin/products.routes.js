381cdbf2ea1047c6ee1c6cbd473d8eb395741e3b
Asad Babayev <esedbabayev12@gmail.com>
2024-11-02 23:24:32 +0400

    export router

--- routes/admin/products.routes.js
+++ routes/admin/products.routes.js
@@ -9,3 +9,5 @@ import { upload } from "../../helpers/cloudinary.js";
 const router = express.Router();
 
 router.post("/upload-image", upload.single("my_file"), uploadImageHandler);
+
+export default router
