fafe500b9440eaed1eca7158da95620b5c2360a0
Asad Babayev <esedbabayev12@gmail.com>
2024-11-02 23:22:23 +0400

    create route for uploading image

--- routes/admin/products.routes.js
+++ routes/admin/products.routes.js
@@ -2,3 +2,10 @@ import express from "express";
 
 // Controllers
 import { uploadImageHandler } from "../../controllers/admin/products.controller.js";
+
+// Helpers
+import { upload } from "../../helpers/cloudinary.js";
+
+const router = express.Router();
+
+router.post("/upload-image", upload.single("my_file"), uploadImageHandler);
