b2025d68597b8ff4a797872cc3c71265bd889797
Asad Babayev <esedbabayev12@gmail.com>
2024-11-03 13:32:05 +0400

    create product routes

--- routes/admin/products.routes.js
+++ routes/admin/products.routes.js
@@ -1,7 +1,13 @@
 import express from "express";
 
 // Controllers
-import { uploadImageHandler } from "../../controllers/admin/products.controller.js";
+import {
+  uploadImageHandler,
+  addProduct,
+  getAllProducts,
+  editProduct,
+  deleteProduct,
+} from "../../controllers/admin/products.controller.js";
 
 // Helpers
 import { upload } from "../../helpers/cloudinary.js";
@@ -9,5 +15,9 @@ import { upload } from "../../helpers/cloudinary.js";
 const router = express.Router();
 
 router.post("/upload-image", upload.single("my_file"), uploadImageHandler);
+router.post("/add", addProduct);
+router.get("/get", getAllProducts);
+router.patch("/edit/:id", editProduct);
+router.delete("/delete/:id", deleteProduct);
 
-export default router
+export default router;
