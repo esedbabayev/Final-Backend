68a5c7caa4bdfc4720e55606c1f60cd59067371b
Asad Babayev <esedbabayev12@gmail.com>
2024-11-03 12:57:26 +0400

    create add product function

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,5 +1,8 @@
 import { imageUpload } from "../../helpers/cloudinary.js";
 
+// Model
+import { Product } from "../../models/product.model.js";
+
 export const uploadImageHandler = async (request, response) => {
   try {
     const b64 = Buffer.from(request.file.buffer).toString("base64");
@@ -15,6 +18,88 @@ export const uploadImageHandler = async (request, response) => {
   } catch (error) {
     console.log(error);
     response.json({
+      success: false,
+      message: "An error occured",
+    });
+  }
+};
+
+// Add product
+
+export const addProduct = async (request, response) => {
+  try {
+    const {
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStock,
+      averageReview,
+    } = request.body;
+
+    const newProduct = new Product({
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStock,
+      averageReview,
+    });
+
+    await newProduct.save();
+
+    response.status(201).json({
+      success: true,
+      data: newProduct,
+    });
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({
+      success: false,
+      message: "An error occured",
+    });
+  }
+};
+
+// Get all products
+
+export const getAllProducts = async (request, response) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({
+      success: false,
+      message: "An error occured",
+    });
+  }
+};
+
+// Edit product
+
+export const editProduct = async (request, response) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({
+      success: false,
+      message: "An error occured",
+    });
+  }
+};
+
+// Delete product
+
+export const deleteProduct = async (request, response) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({
       success: false,
       message: "An error occured",
     });
