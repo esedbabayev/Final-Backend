7ca9db094b70ec1e2c056358aef1e634321dbe98
Asad Babayev <esedbabayev12@gmail.com>
2024-11-08 18:43:20 +0400

    add filter & sort logic

--- controllers/user/products.controller.js
+++ controllers/user/products.controller.js
@@ -3,6 +3,42 @@ import { Product } from "../../models/product.model.js";
 
 export const getFilteredProducts = async (request, response) => {
   try {
+    const {
+      category = [],
+      brand = [],
+      sortBy = "price-lowtohigh",
+    } = request.query;
+
+    let filters = {};
+
+    if (category.length) {
+      filters.category = { $in: category.split(",") };
+    }
+
+    if (brand.length) {
+      filters.brand = { $in: brand.split(",") };
+    }
+
+    let sort = {};
+
+    switch (sortBy) {
+      case "price-lowtohigh":
+        sort.price = 1;
+        break;
+      case "price-hightolow":
+        sort.price = -1;
+        break;
+      case "title-atoz":
+        sort.title = 1;
+        break;
+      case "title-ztoa":
+        sort.title = -1;
+        break;
+      default:
+        sort.price = 1;
+        break;
+    }
+
     const filteredProducts = await Product.find({});
 
     response.status(200).json({
