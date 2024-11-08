eb32aea79434d2dfdc23fe82935625e3f29ba356
Asad Babayev <esedbabayev12@gmail.com>
2024-11-08 23:40:10 +0400

    create route

--- routes/user/products.routes.js
+++ routes/user/products.routes.js
@@ -1,10 +1,14 @@
 import express from "express";
 
 // Controllers
-import { getFilteredProducts } from "../../controllers/user/products.controller.js";
+import {
+  getFilteredProducts,
+  getProductById,
+} from "../../controllers/user/products.controller.js";
 
 const router = express.Router();
 
 router.get("/get", getFilteredProducts);
+router.get("/get/:id", getProductById);
 
 export default router;
