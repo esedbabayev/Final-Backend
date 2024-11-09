b2d772409650e309c6489c7604d8c9590720ceb8
Asad Babayev <esedbabayev12@gmail.com>
2024-11-09 13:42:14 +0400

    create add to cart & fetch cart items funcs

---
+++ controllers/user/cart.controller.js
@@ -0,0 +1,123 @@
+// Models
+import { Cart } from "../../models/cart.model.js";
+import { Product } from "../../models/product.model.js";
+
+export const addToCart = async (request, response) => {
+  try {
+    const { userId, productId, quantity } = request.body;
+
+    if (!userId || !productId || quantity <= 0) {
+      return response
+        .status(400)
+        .json({ success: false, message: "Invalid data provided" });
+    }
+
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return response
+        .status(404)
+        .json({ success: false, message: "Product not found" });
+    }
+
+    let cart = await Cart.findOne({ userId });
+
+    if (!cart) {
+      cart = new Cart({ userId, items: [] });
+    }
+
+    const findCurrentProductIndex = cart.items.findIndex(
+      (item) => item.productId.toString() === productId
+    );
+
+    if (findCurrentProductIndex === -1) {
+      cart.items.push({ productId, quantity });
+    } else {
+      cart.items[findCurrentProductIndex].quantity += quantity;
+    }
+
+    await cart.save();
+    response.status(200).json({
+      success: true,
+      message: "Product added to the cart",
+      data: cart,
+    });
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({
+      success: false,
+      message: "An error occured",
+    });
+  }
+};
+export const getCartItems = async (request, response) => {
+  try {
+    const { userId } = request.params;
+
+    if (!userId) {
+      return response
+        .status(400)
+        .json({ success: false, message: "User id is mandatory" });
+    }
+
+    const cart = await Cart.findOne({ userId }).populate({
+      path: "item.productId",
+      select: "image title price salePrice",
+    });
+
+    if (!cart) {
+      return response
+        .status(404)
+        .json({ success: false, message: "Cart not found" });
+    }
+
+    const validItems = cart.items.filter((product) => product.productId);
+
+    if (validItems.length < cart.items.length) {
+      cart.items = validItems;
+      await cart.save();
+    }
+
+    const populateCartItems = validItems.map((item) => ({
+      productId: item.productId._id,
+      image: item.productId.image,
+      title: item.productId.title,
+      price: item.productId.price,
+      salePrice: item.productId.salePrice,
+      quantity: item.quantity,
+    }));
+
+    response
+      .status(200)
+      .json({
+        success: true,
+        data: { ...cart._doc, items: populateCartItems },
+      });
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({
+      success: false,
+      message: "An error occured",
+    });
+  }
+};
+export const updateCartItemQuantity = async (request, response) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({
+      success: false,
+      message: "An error occured",
+    });
+  }
+};
+export const removeFromCart = async (request, response) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({
+      success: false,
+      message: "An error occured",
+    });
+  }
+};
