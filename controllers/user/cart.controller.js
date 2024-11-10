e9c4103733638b3afa63478790db90b9916235cb
Asad Babayev <esedbabayev12@gmail.com>
2024-11-10 14:54:06 +0400

    add delete from cart func

--- controllers/user/cart.controller.js
+++ controllers/user/cart.controller.js
@@ -140,10 +140,10 @@ export const updateCartItemQuantity = async (request, response) => {
 
     const populateCartItems = cart.items.map((item) => ({
       productId: item.productId ? item.productId._id : null,
-      image: item.image ? item.productId.image : null,
-      title: item.title ? item.productId.title : "Product not found",
-      price: item.price ? item.productId.price : null,
-      salePrice: item.salePrice ? item.productId.salePrice : null,
+      image: item.productId ? item.productId.image : null,
+      title: item.productId ? item.productId.title : "Product not found",
+      price: item.productId ? item.productId.price : null,
+      salePrice: item.productId ? item.productId.salePrice : null,
       quantity: item.quantity,
     }));
 
@@ -162,6 +162,50 @@ export const updateCartItemQuantity = async (request, response) => {
 
 export const removeFromCart = async (request, response) => {
   try {
+    const { userId, productId } = request.params;
+
+    if (!userId || !productId) {
+      return response
+        .status(400)
+        .json({ success: false, message: "Invalid data provided" });
+    }
+
+    const cart = await Cart.findOne({ userId }).populate({
+      path: "item.productId",
+      select: "image title price salePrice",
+    });
+
+    if (!cart) {
+      return response.status(404).json({
+        success: false,
+        message: "Cart not found",
+      });
+    }
+
+    cart.items = cart.items.filter(
+      (item) => item.productId._id.toString() !== productId
+    );
+
+    await cart.save();
+
+    await Cart.populate({
+      path: "item.productId",
+      select: "image title price salePrice",
+    });
+
+    const populateCartItems = cart.items.map((item) => ({
+      productId: item.productId ? item.productId._id : null,
+      image: item.productId ? item.productId.image : null,
+      title: item.productId ? item.productId.title : "Product not found",
+      price: item.productId ? item.productId.price : null,
+      salePrice: item.productId ? item.productId.salePrice : null,
+      quantity: item.quantity,
+    }));
+
+    response.status(200).json({
+      success: true,
+      data: { ...cart._doc, items: populateCartItems },
+    });
   } catch (error) {
     console.log(error);
     response.status(500).json({
