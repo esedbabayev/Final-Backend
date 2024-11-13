3b3f0b50ae2c030e5d96f6b80d92bc832c3ebb73
Asad Babayev <esedbabayev12@gmail.com>
2024-11-14 02:46:01 +0400

    create routes

--- routes/user/order.routes.js
+++ routes/user/order.routes.js
@@ -5,10 +5,13 @@ import {
   capturePayment,
   getAllOrdersByUser,
   getOrderDetails,
-} from "../../controllers/user/order.controller";
+} from "../../controllers/user/order.controller.js";
 
 const router = express.Router();
 
 router.post("/create", createOrder);
+router.post("/capture", capturePayment);
+router.get("/list/:userId", getAllOrdersByUser);
+router.get("/details/:id", getOrderDetails);
 
 export default router;
