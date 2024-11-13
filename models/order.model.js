acc95584582f6c8498a1aa400ad8bb68b184e060
Asad Babayev <esedbabayev12@gmail.com>
2024-11-13 17:32:57 +0400

    create model

---
+++ models/order.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const OrderSchema = new mongoose.Schema({
+  userId: String,
+  cartItems: [
+    {
+      productId: String,
+      title: String,
+      image: String,
+      price: String,
+      quantity: Number,
+    },
+  ],
+  addressInfo: {
+    addressId: String,
+    address: String,
+    city: String,
+    postCode: String,
+    phone: String,
+    notes: String,
+  },
+  orderStatus: String,
+  paymentMethod: String,
+  paymentStatus: String,
+  totalAmount: Number,
+  orderDate: Date,
+  orderUpdateDate: Date,
+  paymentId: String,
+  payerId: String,
+});
+
+export const Order = mongoose.model("Order", OrderSchema);
