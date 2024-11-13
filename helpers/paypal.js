b8c08cb6368988695b60a46479e369b2c7a2e196
Asad Babayev <esedbabayev12@gmail.com>
2024-11-14 02:46:18 +0400

    replace with dotenv

--- helpers/paypal.js
+++ helpers/paypal.js
@@ -1,11 +1,12 @@
 import paypal from "paypal-rest-sdk";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 paypal.configure({
-  mode: "sandbox",
-  client_id:
-    "AV_bD8W3h3tfBwFMZsd96JwrIVBpOCZEgFBV68OJXNmsqDQCisuTBDPeZtc8qB3T9HaY1Kfj8zQUfN0_",
-  client_secret:
-    "EDfE09euc81tMaM4g_uwn4S9UbLZqel5kXnv4Ck1RR1pPIaJVtpIZ2uR9-u0jf4OpTAxfxTWSqlS3oll",
+  mode: process.env.PAYPAL_MODE,
+  client_id: process.env.PAYPAL_CLIENT_ID,
+  client_secret: process.env.PAYPAL_CLIENT_SECRET,
 });
 
 export default paypal;
