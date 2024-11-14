9cf31387cd271f2e64af383dfadfe2e785248102
Asad Babayev <esedbabayev12@gmail.com>
2024-11-15 00:52:56 +0400

    replace w/ patch

--- routes/admin/order.routes.js
+++ routes/admin/order.routes.js
@@ -11,6 +11,6 @@ const router = express.Router();
 
 router.get("/get", getAllOrdersOfAllUsers);
 router.get("/details/:id", getOrderDetailsForAdmin);
-router.put("/update/:id", updateOrderStatus);
+router.patch("/update/:id", updateOrderStatus);
 
 export default router;
