8f3d43d3569452444ff36922c015e484dcd0b8b4
Asad Babayev <esedbabayev12@gmail.com>
2024-11-14 22:06:42 +0400

    create

---
+++ routes/admin/order.routes.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+// Controllers
+import {
+  getAllOrdersOfAllUsers,
+  getOrderDetailsForAdmin,
+  updateOrderStatus,
+} from "../../controllers/admin/order.controller.js";
+
+const router = express.Router();
+
+router.get("/get", getAllOrdersOfAllUsers);
+router.get("/details/:id", getOrderDetailsForAdmin);
+router.put("/update/:id", updateOrderStatus);
+
+export default router;
